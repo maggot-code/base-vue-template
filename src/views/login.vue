ed3579fc7319ac02ce561c02a3c48b04c8bf156e
maggot_code <wowhuanhuan233@163.com>
2021-01-05 00:08:08 +0800

    feat：登录基础页面

---
+++ src/views/login.vue
@@ -0,0 +1,41 @@
+<!--
+ * @Author: maggot-code
+ * @Date: 2021-01-04 21:59:33
+ * @LastEditors: maggot-code
+ * @LastEditTime: 2021-01-04 22:05:06
+ * @Description: login.vue
+-->
+<template>
+    <div class="mg-login">mg-login</div>
+</template>
+
+<script>
+export default {
+    name: "mg-login",
+    components: {},
+    data() {
+        //这里存放数据
+        return {};
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {},
+    //方法集合
+    methods: {},
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() {},
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {},
+    beforeCreate() {}, //生命周期 - 创建之前
+    beforeMount() {}, //生命周期 - 挂载之前
+    beforeUpdate() {}, //生命周期 - 更新之前
+    updated() {}, //生命周期 - 更新之后
+    beforeDestroy() {}, //生命周期 - 销毁之前
+    destroyed() {}, //生命周期 - 销毁完成
+    activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
+};
+</script>
+<style lang='scss' scoped>
+@import "~@/assets/style/login.scss";
+</style>
