135ae312c2fe6634230489c51227d84dbd0eb13c
maggot_code <wowhuanhuan233@163.com>
2021-01-08 17:29:50 +0800

    feat：调整步进器；

--- src/store/nprogress.js
+++ src/store/nprogress.js
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-01-08 09:49:35
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-01-08 13:22:20
+ * @LastEditTime: 2021-01-08 13:34:08
  * @Description: store nprogress
  */
 import NProgress from 'nprogress';
@@ -22,9 +22,6 @@ export const mutations = {
     setStepping(state, stepping) {
         state.stepping = stepping > 1 ? 1.0 : stepping;
 
-        // 持续稳定步进
-        NProgress.inc(1);
-
         if (state.stepping >= 1) {
             NProgress.done(true);
             state.stepping = 0.0;
@@ -36,6 +33,9 @@ export const mutations = {
 
 export const actions = {
     _setStepping({ commit }, stepping) {
+        // 持续稳定步进
+        NProgress.inc(1);
+
         commit('setStepping', stepping)
     }
 };
