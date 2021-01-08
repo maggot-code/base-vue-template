aff449240a2248f9ac17b535540d8620d673330d
maggot_code <wowhuanhuan233@163.com>
2021-01-08 09:17:44 +0800

    feat：加入路由白名单

--- src/utils/router/guard.js
+++ src/utils/router/guard.js
@@ -2,20 +2,28 @@
  * @Author: maggot-code
  * @Date: 2021-01-04 23:14:58
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-01-05 00:05:46
+ * @LastEditTime: 2021-01-07 22:32:40
  * @Description: router beforeEach guard
  */
+import { indexOf } from 'lodash';
 import { getToken } from '@/utils/cache/cookie';
 
+// 路由白名单
+const RouterWhiteList = ['404', 'test'];
+
 const routerFilter = ({ to, from }, token) => {
-    // is power ？
-    if (token && to.name === 'login') {
-        return { name: 'root' }
-    } else if (!token && to.name !== 'login') {
-        // tips
-        return { name: 'login', query: { redirect: to.name } }
-    } else {
+    // is power ?
+    if (indexOf(RouterWhiteList, to.name) >= 0) {
         return {}
+    } else {
+        if (token && to.name === 'login') {
+            return { name: 'root' }
+        } else if (!token && to.name !== 'login') {
+            // tips
+            return { name: 'login', query: { redirect: to.name } }
+        } else {
+            return {}
+        }
     }
 }
 
