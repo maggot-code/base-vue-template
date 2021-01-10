3df6b5a087c1cfa3fb05b2d5b4db11d51680fd7a
maggot_code <wowhuanhuan233@163.com>
2021-01-10 23:59:40 +0800

    feat：test

--- src/components/mg-form/index.js
+++ src/components/mg-form/index.js
@@ -2,11 +2,17 @@
  * @Author: maggot-code
  * @Date: 2021-01-08 17:32:22
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-01-08 17:59:21
+ * @LastEditTime: 2021-01-10 22:27:52
  * @Description: export components mg-form
  */
 import MgRadio from './mg-radio';
+import MgInput from './mg-input';
+import MgPassword from './mg-password';
+import MgTextarea from './mg-textarea';
 
 export {
-    MgRadio
+    MgRadio,
+    MgInput,
+    MgPassword,
+    MgTextarea
 }
--- src/views/test.vue
+++ src/views/test.vue
@@ -2,30 +2,83 @@
  * @Author: maggot-code
  * @Date: 2021-01-07 22:31:36
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-01-10 18:52:33
+ * @LastEditTime: 2021-01-10 23:57:47
  * @Description: test.vue
 -->
 <template>
     <div class="mg-test" ref="testEl">
-        <mg-radio></mg-radio>
+        <el-button @click="submit">默认按钮</el-button>
+        <!-- <mg-radio @onChange="testChenage"></mg-radio>
+        <mg-input @onChange="testChenage"></mg-input>
+        <mg-password @onChange="testChenage"></mg-password>
+        <mg-textarea @onChange="testChenage"></mg-textarea> -->
+        <component
+            v-for="(item, field) in formProps"
+            :key="field"
+            :is="item.componentName"
+            :field="item.field"
+        ></component>
     </div>
 </template>
 
 <script>
-import { MgRadio } from "@/components/mg-form";
+import { MgRadio, MgInput, MgPassword, MgTextarea } from "@/components/mg-form";
 export default {
     name: "mg-test",
-    components: { MgRadio },
+    components: { MgRadio, MgInput, MgPassword, MgTextarea },
     data() {
         //这里存放数据
-        return {};
+        return {
+            formProps: {
+                sex: {
+                    componentName: "mg-radio",
+                    attrs: {
+                        mold: "radio",
+                        radioDefault: "0",
+                        radioEnum: [
+                            {
+                                value: "0",
+                                label: "女",
+                            },
+                            {
+                                value: "1",
+                                label: "男",
+                            },
+                        ],
+                    },
+                },
+                name: {
+                    componentName: "mg-input",
+                    attrs: {
+                        value: "111",
+                    },
+                },
+                password: {
+                    componentName: "mg-password",
+                    attrs: {
+                        value: "222",
+                    },
+                },
+                brief: {
+                    componentName: "mg-textarea",
+                    attrs: {
+                        value: "333",
+                    },
+                },
+            },
+        };
     },
     //监听属性 类似于data概念
     computed: {},
     //监控data中的数据变化
     watch: {},
     //方法集合
-    methods: {},
+    methods: {
+        submit() {},
+        testChenage(val) {
+            console.log(val);
+        },
+    },
     //生命周期 - 创建完成（可以访问当前this实例）
     created() {},
     //生命周期 - 挂载完成（可以访问DOM元素）
@@ -41,6 +94,7 @@ export default {
 </script>
 <style lang='scss' scoped>
 .mg-test {
-    width: 200px;
+    width: 80%;
+    margin: 0 auto;
 }
 </style>
