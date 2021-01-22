cec51a857ba8d79001a268044362ec4342af2323
maggot_code <wowhuanhuan233@163.com>
2021-01-22 11:18:52 +0800

    fix：update

--- src/views/test.vue
+++ src/views/test.vue
@@ -2,7 +2,7 @@
  * @Author: maggot-code
  * @Date: 2021-01-07 22:31:36
  * @LastEditors: maggot-code
- * @LastEditTime: 2021-01-19 15:13:58
+ * @LastEditTime: 2021-01-22 11:16:18
  * @Description: test.vue
 -->
 <template>
@@ -22,7 +22,8 @@
 // import schemaJSON from "@/components/mg-form/test/select-schema";
 // import schemaJSON from "@/components/mg-form/test/cascader-schema";
 // import schemaJSON from "@/components/mg-form/test/cascader-schema2";
-import schemaJSON from "@/components/mg-form/test/date-schema";
+// import schemaJSON from "@/components/mg-form/test/date-schema";
+import schemaJSON from "@/components/mg-form/test/transfer-schema";
 import { objectRepeat } from "@/utils/tool";
 import MgForm from "@/components/mg-form";
 export default {
